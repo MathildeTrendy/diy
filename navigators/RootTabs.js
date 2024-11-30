147c344f6f14c03bdbd68576eb22d9f3acf95c67
gottbrecht <simo79j5@stud.kea.dk>
2024-11-30 11:21:51 +0100

    RootTabs opdateret

--- navigators/RootTabs.js
+++ navigators/RootTabs.js
@@ -1,51 +1,61 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Home } from "./../screens";
+import { Home } from "../screens";
 import { colors } from "../config/theme";
-import { setStatusBarBackgroundColor } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 import { AntDesign, Feather } from "@expo/vector-icons";
+import React, { useEffect } from "react";
+import HomeStack from "./HomeStack";
 
 const Tab = createBottomTabNavigator();
 
 const RootTabs = () => {
+  useEffect(() => {}, []);
+
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarStyle: {
-          backgroundColor: colors?.primary,
-          borderTopColor: colors?.secondary,
-          borderTopWidth: 2,
-          height: 60,
-        },
-        tabBarItemStyle: {
-          paddingVertical: 5,
-        },
-        tabBarInactiveTintColor: colors?.tertiary + "cc",
-        tabBarActiveTintColor: colors?.accent + "cc",
-        tabBarIcon: ({ size, color }) => {
-          let iconName;
+    <>
+      <StatusBar style="light" backgroundColor={colors?.primary} />
 
-          if (route.name === "Home") {
-            iconName = "home";
-            return <AntDesign name={iconName} size={size + 2} color={color} />;
-          } else if (route.name === "Search") {
-            iconName = "search";
-          } else if (route.name === "Cart") {
-            iconName = "shopping-cart";
-          } else if (route.name === "Profile") {
-            iconName = "user";
-          }
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarStyle: {
+            backgroundColor: colors?.primary,
+            borderTopColor: colors?.secondary,
+            borderTopWidth: 2,
+            height: 60,
+          },
+          tabBarItemStyle: {
+            paddingVertical: 5,
+          },
+          tabBarInactiveTintColor: colors?.tertiary + "cc",
+          tabBarActiveTintColor: colors?.accent + "cc",
+          tabBarIcon: ({ size, color }) => {
+            let iconName;
 
-          return <Feather name={iconName} size={size} color={color} />;
-        },
-        headerShown: false,
-        tabBarHideOnKeyboard: true,
-      })}
-    >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Home} />
-      <Tab.Screen name="Cart" component={Home} />
-      <Tab.Screen name="Profile" component={Home} />
-    </Tab.Navigator>
+            if (route.name === "Home") {
+              iconName = "home";
+              return (
+                <AntDesign name={iconName} size={size + 2} color={color} />
+              );
+            } else if (route.name === "Search") {
+              iconName = "search";
+            } else if (route.name === "Cart") {
+              iconName = "shopping-cart";
+            } else if (route.name === "Profile") {
+              iconName = "user";
+            }
+
+            return <Feather name={iconName} size={size} color={color} />;
+          },
+          headerShown: false,
+          tabBarHideOnKeyboard: true,
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Search" component={Home} />
+        <Tab.Screen name="Cart" component={Home} />
+        <Tab.Screen name="Profile" component={Home} />
+      </Tab.Navigator>
+    </>
   );
 };
 
