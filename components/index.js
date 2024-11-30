d6c09fa5194e2cd99e26e717733e59326e1bec8f
gottbrecht <simo79j5@stud.kea.dk>
2024-11-30 11:50:56 +0100

    packages updated

--- components/index.js
+++ components/index.js
@@ -3,6 +3,7 @@ import StyledButton from "./Buttons/StyledButton";
 import StyledTextInput from "./Inputs/StyledTextInput";
 import MainContainer from "./Containers/MainContainer";
 import ScrollableMainContainer from "./Containers/ScrollableMainContainer";
+import SectionHeader from "./Texts/SectionHeader";
 import DisplayCard from "./Cards/DisplayCard";
 
 export {
