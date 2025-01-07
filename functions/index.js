eade68e8ffa8db375022811c8d587eb1f9514bfa
MathildeTrendy <math38v6@stud.kea.dk>
2025-01-07 15:07:32 +0100

    stripe udkommenteret

--- functions/index.js
+++ functions/index.js
@@ -10,26 +10,8 @@ require('dotenv').config();
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Brug miljøvariabel for Stripe secret key
 
-// Firebase Function til at oprette Payment Intent
-exports.createPaymentIntent = onRequest(async (req, res) => {
-    try {
-      const amount = req.body.amount; // Beløbet skal sendes fra frontend
-        
-        // Simulér succesfuld betaling
-    const paymentResponse = {
-        status: 'succeeded',
-        message: `Betaling på ${amount} DKK gennemført succesfuldt.`,
-      };
-      
 
-    res.status(200).send(paymentResponse);
-    } catch (error) {
-      logger.error('Error creating payment intent:', error);
-      res.status(500).send({ error: error.message });
-    }
-  });
 
 
 // Create and deploy your first functions
