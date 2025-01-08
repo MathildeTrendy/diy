fb7af717c07e79d746ecc0494aae64493edd033f
MathildeTrendy <math38v6@stud.kea.dk>
2025-01-08 20:59:19 +0100

    deploy firebase

---
+++ README.md
@@ -0,0 +1,8 @@
+# Deploy guide
+
+Sørg for at være i diy mappen i terminalen
+
+1. Build med "npx expo export ."
+2. Deploy til firebase med "firebase deploy --only hosting"
+
+url: https://retrove-abd5d.web.app
--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,6 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-require('dotenv').config();
-
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
 
 
